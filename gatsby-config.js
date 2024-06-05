566178c51b177900ebc8c6f3c18d3af67c3c8ac3
Nathan Flinchum <nathanlf@ad.renci.org>
2024-06-05 12:54:31 -0400

    tutorial site update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,30 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+// require("dotenv").config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// });
+
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    // {
+    //   resolve: "gatsby-source-google-docs",
+    //   options: {
+    //     // https://drive.google.com/drive/folders/1ryiW1fQLR-xiHm9Zh2-llnCQQw9aGeZU
+    //     folder: "1ryiW1fQLR-xiHm9Zh2-llnCQQw9aGeZU",
+    //     createPages: true
+    //   },
+    // }, "gatsby-plugin-mdx",
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `pages`,
+    //     path: `./src/pages`,
+    //   },
+    // },
+  ],
 }
